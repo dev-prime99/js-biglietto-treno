5a95d83345594092e5d2ce12f45506b0398b26e8
dev-prime99 <prime99@icloud.com>
2019-10-19 18:37:01 +0200

    complited bonus ex

--- BONUS/assets/js/main.js
+++ BONUS/assets/js/main.js
@@ -10,29 +10,39 @@ function calcolo(){
   eta=parseInt(document.getElementById("eta").value);
 
   // Prezzo finale
-  prezzo = km * 0.21;
+  prezzo = (km * 0.21).toFixed(2);
 
   // Sconto Minorenne
-  mino = (prezzo / 100) * 20;
+  mino = ((prezzo / 100) * 20).toFixed(2);
 
   // Sconto Over
-  over = (prezzo / 100) * 40;
+  over = ((prezzo / 100) * 40).toFixed(2);
 
   // Calcolo costo finale
-  if (eta < 18){
-    finale = prezzo - mino;
-    document.getElementById("sconto_").innerText= mino + " €";
-  } else if (eta > 65){
-    finale = prezzo - over;
-    document.getElementById("sconto_").innerText= over + " €";
+  if ((km > 0) && (eta >= 0)) {
+    if (eta < 18){
+      finale = (prezzo - mino);
+      document.getElementById("sconto_").innerText= mino + " €";
+    } else if (eta > 65){
+      finale = prezzo - over;
+      document.getElementById("sconto_").innerText= over + " €";
+    } else{
+      finale = prezzo;
+      document.getElementById("sconto_").innerText= "/";
+    }
+    document.getElementById("prezzo_").innerText=finale + " €";
+    //document.getElementById("h2").style.display = "block";
   } else{
-    finale = prezzo;
-    document.getElementById("sconto_").innerText= "/";
+    document.getElementById("box").style.display = "flex";
+    document.getElementById("normal").style.display = "none";
+    document.getElementById('km').value = '';
+    document.getElementById('eta').value = '';
+    document.getElementById("sconto_").innerText= "Errore";
+    document.getElementById("prezzo_").innerText= "Errore";
   }
+}
 
-  // Scrittura Risultato
-  document.getElementById("prezzo_").innerText=finale + " €";
-  console.log(finale);
-
-
+function ritorno(){
+  document.getElementById("box").style.display = "none";
+  document.getElementById("normal").style.display = "flex";
 }
